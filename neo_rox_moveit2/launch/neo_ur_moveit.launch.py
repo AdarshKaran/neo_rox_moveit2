6a45a67a2d52c925dea3aaa36afc65dce27e4a29
Adarsh Karan Kesavadas Prasanth <adarshkaran01@gmail.com>
2025-02-04 13:47:51 +0530

    Cleanup configs, dynamic param substitutions for UR arms and updates to launch file

--- neo_rox_moveit2/launch/neo_ur_moveit.launch.py
+++ neo_rox_moveit2/launch/neo_ur_moveit.launch.py
@@ -28,6 +28,7 @@
 
 #
 # Author: Pradheep Padmanabhan
+# Contributor: Adarsh Karan K P
 
 import os
 
@@ -43,44 +44,64 @@ from launch_ros.descriptions import ParameterValue, ParameterFile
 from moveit_configs_utils import MoveItConfigsBuilder
 from pathlib import Path
 from neo_rox_moveit2.launch_common import load_yaml
+from launch.event_handlers import OnProcessExit
+from launch.actions import RegisterEventHandler
 
 def launch_setup(context, *args, **kwargs):
 
     # Initialize Arguments
-    ur_type = LaunchConfiguration("ur_type")
+    arm_type = LaunchConfiguration("arm_type")
     # General arguments
     moveit_config_package = LaunchConfiguration("moveit_config_package")
     prefix = LaunchConfiguration("prefix")
     use_sim_time = LaunchConfiguration("use_sim_time")
     launch_rviz = LaunchConfiguration("launch_rviz")
     ur_dc = LaunchConfiguration("use_ur_dc")
+    use_gz = LaunchConfiguration("use_gz")
     
     urdf = os.path.join(get_package_share_directory('rox_description'),
         'urdf',
         'rox.urdf.xacro')
-
+    
     # MoveIt Configuration
     srdf = os.path.join(get_package_share_directory('neo_rox_moveit2'),
         'srdf',
         'rox.srdf.xacro')
 
-    # Trajectory Execution Configuration
+    # Controllers Configuration
     controllers_yaml = os.path.join(
         get_package_share_directory(str(moveit_config_package.perform(context))),
         "config",
-        str(ur_type.perform(context)),
         "controllers.yaml",
     )
-
+    controllers_yaml_with_substitutions = ParameterFile(controllers_yaml, allow_substs=True)
+    # Evaluate the parameter file to apply dynamic substitutions
+    controllers_yaml_with_substitutions.evaluate(context)
+    controllers_yaml = os.path.join(
+        get_package_share_directory('neo_rox_moveit2'),
+        "config",
+        str(controllers_yaml_with_substitutions.param_file)
+    )
+    
+    # Joint Limits Configuration
     joint_limits_yaml = os.path.join(
-            get_package_share_directory('neo_rox_moveit2'),
-            "config",
-            str(ur_type.perform(context)),
-            "joint_limits.yaml")
+        get_package_share_directory(str(moveit_config_package.perform(context))),
+        "config",
+        "joint_limits.yaml",
+    )
+    joint_limits_yaml_with_substitutions = ParameterFile(joint_limits_yaml, allow_substs=True)
+    # Evaluate the parameter file to apply dynamic substitutions
+    joint_limits_yaml_with_substitutions.evaluate(context)
+    joint_limits_yaml = os.path.join(
+        get_package_share_directory('neo_rox_moveit2'),
+        "config",
+        str(joint_limits_yaml_with_substitutions.param_file)
+    )
 
     moveit_config = (
         MoveItConfigsBuilder(robot_name="rox_ur", package_name="neo_rox_moveit2")
-        .robot_description_semantic(file_path=srdf, mappings={"prefix": ur_type})
+        .robot_description_semantic(file_path=srdf, mappings={"prefix": prefix})
+        .robot_description(file_path=urdf, mappings={"arm_type": arm_type, "use_gz": use_gz, "use_ur_dc": ur_dc, "force_abs_paths": "true"})
         .trajectory_execution(file_path=controllers_yaml)
         .joint_limits(file_path=joint_limits_yaml)
         .to_moveit_configs()
@@ -119,20 +140,36 @@ def launch_setup(context, *args, **kwargs):
             },
         ],
     )
-    nodes_to_start = [move_group_node, rviz_node]
+
+    wait_robot_description = Node(
+        package="ur_robot_driver",
+        executable="wait_for_robot_description",
+        output="screen",
+    )
+
+    handler = RegisterEventHandler(
+        OnProcessExit(
+            target_action=wait_robot_description,
+            on_exit=[move_group_node, rviz_node],
+        )
+    )
+
+    nodes_to_start = [wait_robot_description, handler]
 
     return nodes_to_start
-
 
 def generate_launch_description():
 
     declared_arguments = []
     # UR specific arguments
+
     declared_arguments.append(
         DeclareLaunchArgument(
-            "ur_type",
-            description="Type/series of used UR robot.",
-            choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20"],
+            'arm_type', 
+            default_value='',
+            description='Arm Types:\n'
+                        '\t Elite Arms: ec66, cs66\n'
+                        '\t Universal Robotics (UR): ur5, ur10, ur5e, ur10e' 
         )
     )
     declared_arguments.append(
@@ -166,6 +203,14 @@ def generate_launch_description():
             "use_ur_dc",
             default_value="false",
             description="Uses a shorter cabin box. Check with Neobotix, before selecting.",
+        )
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_gz",
+            default_value="false",
+            description="Whether to enable Gazebo simulation.",
         )
     )
 
